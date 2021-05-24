346450e92d07029e66da3052db8d638e4755b26a
nlyson <72270566+nlyson@users.noreply.github.com>
2021-05-24 11:05:36 -0500

    Update EllieNameController.cs

--- OllieAndEllie/EllieNameController.cs
+++ OllieAndEllie/EllieNameController.cs
@@ -1,3 +1,8 @@
+/*
+    Description: This script keeps the camera position at a given vertical offset.
+    Rotation/Scale are fixed so those are set in the Unity editor.
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +10,7 @@ using UnityEngine;
 public class EllieNameController : MonoBehaviour
 {
     public GameObject player;
-    public float offset = 5.0f;
+    public float offset = 5.0f;     // Offset is public so it can be altered in the Unity editor
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,7 @@ public class EllieNameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Each frame, position the camera so that is offset units above the player.
         transform.position = player.transform.position + new Vector3(0.0f, offset, 0.0f);
     }
 }
